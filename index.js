53770048cffee20f96fbabf66b1777f2513d8006
Daniel Leskosky <dleskosky@gmail.com>
2020-07-04 18:48:58 -0400

    Commit for homepage

--- index.js
+++ index.js
@@ -33,8 +33,12 @@ let notes = [
   }
 ]
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
 })
 
 const generateId = () => {
