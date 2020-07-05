c86cfa9a6e7b8c682b11a42f8a12cf163555f3f1
Daniel Leskosky <dleskosky@gmail.com>
2020-07-04 20:04:05 -0400

    Commit for production build

---
+++ build/precache-manifest.fec334fe0305a9eee7b4cf975d627f20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6baf3e2ed3b653879531523453969c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b559b7821b82ac2db355",
+    "url": "/static/css/main.6a943033.chunk.css"
+  },
+  {
+    "revision": "94aade0c79826a3b7237",
+    "url": "/static/js/2.9fab9d17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fab9d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b559b7821b82ac2db355",
+    "url": "/static/js/main.6fc6b031.chunk.js"
+  },
+  {
+    "revision": "2855edd9aece52c5da3e",
+    "url": "/static/js/runtime-main.59a4d97e.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 let notes = [
   {
