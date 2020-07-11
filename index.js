85859d663153a7bb2b2fae28edcc31d036d4eb9d
Daniel Leskosky <dleskosky@gmail.com>
2020-07-11 12:39:41 -0400

    Commit for moving error handling into middleware

--- index.js
+++ index.js
@@ -1,58 +1,19 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+require('dotenv').config()
 const Note = require('./models/note')
 
 const cors = require('cors')
 
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(express.static('build'))
-
-let notes = [
-  {
-    "id": 1,
-    "content": "HTML is easy",
-    "date": "2019-05-30T17:30:31.098Z",
-    "important": true
-  },
-  {
-    "id": 2,
-    "content": "Browser can execute only Javascript",
-    "date": "2019-05-30T17:30:31.098Z",
-    "important": true
-  },
-  {
-    "id": 3,
-    "content": "GET and POST are the most important methods of HTTP protocol",
-    "date": "2019-05-30T17:30:31.098Z",
-    "important": true
-  },
-  {
-    "id": 4,
-    "content": "POST is used to add data to a REST api",
-    "date": "2019-05-30T17:30:31.098Z",
-    "important": true
-  }
-]
-
-/*app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})*/
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
 })
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -68,22 +29,62 @@ app.post('/api/notes', (request, response) => {
     })
 
     note.save().then(savedNote => {
-      response.json(savedNote)
+      response.json(savedNote.toJSON())
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note.toJSON())
-  })  
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note.toJSON())
+      } else {
+      response.status(404).end()
+      }
+    }) 
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id) 
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
