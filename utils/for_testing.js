871bda0da1205da91844051465824909bd0698f2
Daniel Leskosky <dleskosky@gmail.com>
2020-07-17 20:52:02 -0400

    Commit for testing with jest

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -5,15 +5,15 @@ const palindrome = (string) => {
       .join('')
   }
   
-  const average = (array) => {
-    const reducer = (sum, item) => {
-      return sum + item
-    }
-  
-    return array.length === 0
-        ? 0
-        : array.reduce(reducer, 0) / array.length
+const average = (array) => {
+  const reducer = (sum, item) => {
+    return sum + item
   }
+  
+  return array.length === 0
+    ? 0
+    : array.reduce(reducer, 0) / array.length
+}
   
   module.exports = {
     palindrome,
